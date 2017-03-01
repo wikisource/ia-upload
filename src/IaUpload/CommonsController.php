4c1747829a9df4d3e83907eb88aab5b2a15aa71e
Sam Wilson <sam@samwilson.id.au>
2017-03-01 09:26:51 +0800

    Remove unicode 200E from category name

--- src/IaUpload/CommonsController.php
+++ src/IaUpload/CommonsController.php
@@ -470,7 +470,7 @@ class CommonsController {
 		$content .= '| Homecat      = ' . "\n";
 		$content .= '}}' . "\n" . '{{Djvu}}' . "\n\n";
 		$content .= '== {{int:license-header}} ==' . "\n" . '{{PD-scan}}' . "\n\n";
-		$content .= '[[Category:Uploaded with IA Upload‎]]' . "\n";
+		$content .= '[[Category:Uploaded with IA Upload]]' . "\n";
 
 		$isCategorised = false;
 		if ( isset( $data['metadata']['date'][0] ) && $this->commonsClient->pageExist( 'Category:' . $data['metadata']['date'][0] . ' books' ) ) {
