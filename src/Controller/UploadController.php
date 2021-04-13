wikisource/ia-upload

commit 43d62d7148727b11e579ff176de201f9a02b13c8

Permit files to reference existing IA identifiers
Allow duplicate IA identifiers to be uploaded (even for the
same format), and make a better error message for when an
uploaded file is an exact duplicate of an existing file.

Also add var-dumper because it's useful for development!

Bug: https://phabricator.wikimedia.org/T269518

src/Controller/UploadController.php
@@ -10,6 +10,7 @@ use Mediawiki\Api\Guzzle\ClientFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteParser;
+use Wikimedia\SimpleI18n\I18nContext;
 use Wikisource\IaUpload\ApiClient\CommonsClient;
 use Wikisource\IaUpload\ApiClient\IaClient;
 use Wikisource\IaUpload\OAuth\MediaWikiOAuth;
@@ -270,8 +271,7 @@ class UploadController {
 		// See if the filename already exists on Commons.
 		$fullCommonsName = $commonsName . '.' . $format;
 		if ( $this->commonsClient->pageExist( 'File:' . $fullCommonsName ) ) {
-			$url = $this->config['wiki_base_url'] . '/wiki/File:' . rawurlencode( $fullCommonsName );
-			$link = "<a href='$url'>" . htmlspecialchars( $fullCommonsName ) . '</a>';
+			$link = $this->commonsClient->getHtmlLink( 'File:' . $fullCommonsName );
 			return $this->outputsInitTemplate( [
 				'iaId' => $iaId,
 				'format' => $format,
@@ -283,14 +283,8 @@ class UploadController {
 		// See if a page for the IA item exists on Commons under a different name.
 		$existingPage = $this->commonsClient->pageForIAItem( $iaId );
 		if ( $existingPage ) {
-			$url = $this->config['wiki_base_url'] . '/wiki/' . rawurlencode( $existingPage );
-			$link = "<a href='$url'>" . htmlspecialchars( $iaId ) . '</a>';
-			return $this->outputsInitTemplate( [
-				'iaId' => $iaId,
-				'format' => $format,
-				'commonsName' => $commonsName,
-				'error' => $this->i18n->message( 'already-on-commons', [ $link ] ),
-			], $response );
+			$linkExisting = $this->commonsClient->getHtmlLink( $existingPage );
+			$warning = $this->i18n->message( 'ia-identifier-exists', [ $iaId, $linkExisting ] );
 		}
 
 		// Output the page.
@@ -340,8 +334,7 @@ class UploadController {
 
 		// Check again that the Commons file doesn't exist.
 		if ( $this->commonsClient->pageExist( 'File:' . $jobInfo['fullCommonsName'] ) ) {
-			$url = 'http://commons.wikimedia.org/wiki/File:' . rawurlencode( $jobInfo['fullCommonsName'] );
-			$link = '<a href="' . $url . '">' . htmlspecialchars( $jobInfo['fullCommonsName'] ) . '</a>';
+			$link = $this->commonsClient->getHtmlLink( 'File:' . $jobInfo['fullCommonsName'] );
 			$jobInfo['error'] = $this->i18n->message( 'already-on-commons', [ $link ] );
 			return $this->outputsFillTemplate( $jobInfo, $response );
 		}
@@ -385,12 +378,18 @@ class UploadController {
 				if ( $jobInfo['format'] === 'djvu' && $jobInfo['removeFirstPage'] ) {
 					$this->iaClient->removeFirstPage( $localFile );
 				}
-				$this->commonsClient->upload(
+				$result = $this->commonsClient->upload(
 					$jobInfo['fullCommonsName'],
 					$localFile,
 					$jobInfo['description'],
 					'Importation from Internet Archive via [[toollabs:ia-upload|IA-upload]]'
 				);
+				if ( isset( $result['upload']['warnings']['duplicate'][0] ) ) {
+					$dupeFile = $result['upload']['warnings']['duplicate'][0];
+					$dupeLink = $this->commonsClient->getHtmlLink( 'File:' . $dupeFile );
+					$jobInfo['error'] = $this->i18n->message( 'duplicate-on-commons', [ $dupeLink ] );
+					return $this->outputsFillTemplate( $jobInfo, $response );
+				}
 			} catch ( Exception $e ) {
 				unlink( $localFile );
 				rmdir( $jobDirectory );
@@ -406,12 +405,9 @@ class UploadController {
 			}
 			unlink( $localFile );
 			rmdir( $jobDirectory );
-			$url = $this->config['wiki_base_url']
-				. '/wiki/File:'
-				. rawurlencode( $jobInfo['fullCommonsName'] );
-			$msgParam = '<a href="' . $url . '">' . $jobInfo['fullCommonsName'] . '</a>';
+			$fileLink = $this->commonsClient->getHtmlLink( "File:$jobInfo" );
 			return $this->outputsInitTemplate( [
-				'success' => $this->i18n->message( 'successfully-uploaded', [ $msgParam ] ),
+				'success' => $this->i18n->message( 'successfully-uploaded', [ $fileLink ] ),
 			], $response );
 		}
 	}

src/ApiClient/CommonsClient.php
@@ -4,9 +4,7 @@ namespace Wikisource\IaUpload\ApiClient;
 
 use GuzzleHttp\Client;
 use Mediawiki\Api\MediawikiApi;
-use Mediawiki\Api\MediawikiFactory;
 use Mediawiki\Api\SimpleRequest;
-use Mediawiki\Api\UsageException;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -18,6 +16,9 @@ use Psr\Log\LoggerInterface;
  * @license GPL-2.0-or-later
  */
 class CommonsClient {
+
+	/** @var string */
+	private $baseUrl;
 
 	/**
 	 * @var Client
@@ -31,14 +32,26 @@ class CommonsClient {
 
 	/**
 	 * CommonsClient constructor.
-	 * @param string $base_url The wiki base URL with no trailing slash or path.
+	 * @param string $baseUrl The wiki base URL with no trailing slash or path.
 	 * @param Client $oauthClient The Oauth client.
 	 * @param LoggerInterface $logger The logger.
 	 */
-	public function __construct( $base_url, Client $oauthClient, LoggerInterface $logger ) {
+	public function __construct( string $baseUrl, Client $oauthClient, LoggerInterface $logger ) {
+		$this->baseUrl = rtrim( $baseUrl, '/' );
 		$this->client = $oauthClient;
-		$this->mediawikiApi = new MediawikiApi( $base_url . '/w/api.php', $oauthClient );
+		$this->mediawikiApi = new MediawikiApi( $this->baseUrl . '/w/api.php', $oauthClient );
 		$this->mediawikiApi->setLogger( $logger );
+	}
+
+	/**
+	 * Get an HTML anchor element linking to the given page on Wikimedia Commons.
+	 *
+	 * @param string $pageName
+	 * @return string
+	 */
+	public function getHtmlLink( string $pageName ): string {
+		$url = $this->baseUrl . '/wiki/' . rawurlencode( $pageName );
+		return '<a href="' . $url . '">' . htmlspecialchars( $pageName ) . '</a>';
 	}
 
 	/**
@@ -85,20 +98,17 @@ class CommonsClient {
 	}
 
 	/**
-	 * Returns the edit token for the current user
+	 * Upload a file to Commons.
 	 *
 	 * @param string $fileName the name of the file to upload
 	 * @param string $filePath the path to the file
 	 * @param string $text the content of the description page
 	 * @param string $comment an edit comment
 	 * @return array
-	 * @throws UsageException If there's an API error.
 	 */
-	public function upload( $fileName, $filePath, $text, $comment ) {
-		$factory = new MediawikiFactory( $this->mediawikiApi );
-		$fileUploader = $factory->newFileUploader();
-		$fileUploader->setChunkSize( 90 * 1024 * 1024 );
-		$fileUploader->upload( $fileName, $filePath, $text, $comment );
+	public function upload( string $fileName, string $filePath, string $text, string $comment ) {
+		$fileUploader = new CommonsFileUploader( $this->mediawikiApi );
+		return $fileUploader->uploadWithResult( $fileName, $filePath, $text, $comment );
 	}
 
 	/**

src/ApiClient/CommonsFileUploader.php
@@ -1,0 +1,42 @@
+<?php
+
+namespace Wikisource\IaUpload\ApiClient;
+
+use Mediawiki\Api\MediawikiApi;
+use Mediawiki\Api\Service\FileUploader;
+use Mediawiki\Api\SimpleRequest;
+
+/**
+ * A wrapper for FileUploader::upload() that lets us get the API response.
+ * @TODO Remove after https://github.com/addwiki/addwiki/issues/86 is resolved.
+ */
+class CommonsFileUploader extends FileUploader {
+
+	/**
+	 * @param MediawikiApi $api
+	 */
+	public function __construct( MediawikiApi $api ) {
+		parent::__construct( $api );
+		$this->setChunkSize( 90 * 1024 * 1024 );
+	}
+
+	/**
+	 * @param string $targetName
+	 * @param string $location
+	 * @param string $text
+	 * @param string $comment
+	 * @return mixed
+	 */
+	public function uploadWithResult( string $targetName, string $location, string $text = '', string $comment = '' ) {
+		$params = [
+			'filename' => $targetName,
+			'token' => $this->api->getToken(),
+			'text' => $text,
+			'comment' => $comment,
+			'filesize' => filesize( $location ),
+			'file' => fopen( $location, 'r' ),
+		];
+		$params = $this->uploadByChunks( $params );
+		return $this->api->postRequest( new SimpleRequest( 'upload', $params ) );
+	}
+}

commit 729079cbdec012559a31d93ab13ba55170f5ff90

Fix link on successful upload message
A regression introduced in 43d62d7148727b11e579ff176de201f9a02b13c8

Bug: https://phabricator.wikimedia.org/T280038

src/Controller/UploadController.php
@@ -405,7 +405,7 @@ class UploadController {
 			}
 			unlink( $localFile );
 			rmdir( $jobDirectory );
-			$fileLink = $this->commonsClient->getHtmlLink( "File:$jobInfo" );
+			$fileLink = $this->commonsClient->getHtmlLink( 'File:' . $jobInfo['fullCommonsName'] );
 			return $this->outputsInitTemplate( [
 				'success' => $this->i18n->message( 'successfully-uploaded', [ $fileLink ] ),
 			], $response );